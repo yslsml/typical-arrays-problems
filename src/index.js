a10521dd6003d989dbdbd325f2f861271cdb06d1
yslsml <mmf.antonovam@gmail.com>
2021-03-13 16:47:34 +0300

    add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array==0 || typeof(array)=="undefined") return 0
+  let min=array[0]
+  for (let i=0;i<array.length;i++){
+    if (min>array[i]) min=array[i]
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array==0 || typeof(array)=="undefined") return 0
+  let max=array[0]
+  for (let i=0;i<array.length;i++){
+    if (max<array[i]) max=array[i]
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array==0 || typeof(array)=="undefined") return 0
+  let avg=0, sum=0, n=0
+  for (let i=0;i<array.length;i++){
+    sum+=array[i]
+    n++
+  }
+  return avg=sum/n;
 }
